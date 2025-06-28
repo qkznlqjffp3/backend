0cbc9caa6b1c63f2461922cdad382047c491ba5c
Kimseounggue <seounggue@gmail.com>
2025-06-28 14:00:09 +0900

    project init

---
+++ src/main/java/team3/goodbyebug/GoodbyebugApplication.java
@@ -0,0 +1,13 @@
+package team3.goodbyebug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodbyebugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodbyebugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team3/goodbyebug/GoodbyebugApplicationTests.java
@@ -0,0 +1,13 @@
+package team3.goodbyebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodbyebugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
